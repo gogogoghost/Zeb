bf8c116bca9dfec6830a351efb86351da509c737
gogogoghost <ohleach@gmail.com>
2023-09-12 15:39:52 +0800

    fix: promise reject throw from zeb rather than context

--- web/lib/zeb.js
+++ web/lib/zeb.js
@@ -1,5 +1,5 @@
-import { num2arr, concatArr, arr2num, arr2float,makeBuffer,randomString } from "./utils";
-import {Base64} from 'js-base64'
+import { num2arr, concatArr, arr2num, arr2float, makeBuffer, randomString } from "./utils";
+import { Base64 } from 'js-base64'
 
 // js请求native的数据类型
 const REQT = {
@@ -15,7 +15,7 @@ const REQT = {
     BYTEARRAY: 12,
     ARRAY: 13,
     ERROR: 15,
-    JSON:16
+    JSON: 16
 }
 // native响应的数据类型
 const REST = {
@@ -31,7 +31,7 @@ const REST = {
     ARRAY: 13,
     PROMISE: 14,
     ERROR: 15,
-    JSON:16
+    JSON: 16
 }
 
 // 异步回调消息处理
@@ -51,8 +51,8 @@ const objectMap = {}
 //存储待pending的promise
 const promiseMap = {}
 
-const textEncoder=new TextEncoder()
-const textDecoder=new TextDecoder()
+const textEncoder = new TextEncoder()
+const textDecoder = new TextDecoder()
 
 //服务存储
 let apiInternal = {}
@@ -65,38 +65,38 @@ const naviveErrorPrefix = "Native exception:"
 
 // 扩展error方法
 // chrome>=93 支持cause
-Error.prototype.toStr=function(){
-    let msg=""
-    let cause=this
-    while(cause){
-        if(msg){
+Error.prototype.toStr = function () {
+    let msg = ""
+    let cause = this
+    while (cause) {
+        if (msg) {
             msg += " -> "
         }
         // 非error类型，转成字符串
-        if(cause.constructor!=Error){
+        if (cause.constructor != Error) {
             msg += JSON.stringify(cause)
             break
         }
-        msg+=cause.name+":"+cause.message
-        cause=cause.cause
+        msg += cause.name + ":" + cause.message
+        cause = cause.cause
     }
     return msg
 }
 
 // 内存释放注册器
 // chrome >= 84
-let register=null
-if(window.FinalizationRegistry){
-    register=new FinalizationRegistry((token)=>{
+let register = null
+if (window.FinalizationRegistry) {
+    register = new FinalizationRegistry((token) => {
         window.zeb.releaseObject(token)
     })
-}else{
+} else {
     console.warn("Not support FinalizationRegistry. Js object receive from native will be not released.")
 }
 
-function isObjHasFunction(obj){
-    for(const key in obj){
-        if(obj[key] instanceof Function){
+function isObjHasFunction (obj) {
+    for (const key in obj) {
+        if (obj[key] instanceof Function) {
             return true
         }
     }
@@ -104,20 +104,20 @@ function isObjHasFunction(obj){
 }
 
 //obj -> bytes -> base64
-function encodeArg(arg) {
-    if (arg == null || arg == undefined){
+function encodeArg (arg) {
+    if (arg == null || arg == undefined) {
         return num2arr(REQT.NULL, 1)
     }
     const c = arg.constructor
     if (c == Number) {
         if (arg % 1 == 0) {
-            if(arg > 0xffffffff){
+            if (arg > 0xffffffff) {
                 //use 8 bytes
                 return concatArr(
                     num2arr(REQT.INT, 1),
                     num2arr(arg, 8)
                 )
-            }else{
+            } else {
                 //use 4 bytes
                 return concatArr(
                     num2arr(REQT.INT, 1),
@@ -126,8 +126,8 @@ function encodeArg(arg) {
             }
         } else {
             //use 8 bytes
-            const {buf,view}=makeBuffer(REQT.FLOAT,8)
-            view.setFloat64(1,arg)
+            const { buf, view } = makeBuffer(REQT.FLOAT, 8)
+            view.setFloat64(1, arg)
             return buf
         }
     } else if (c == String) {
@@ -144,18 +144,18 @@ function encodeArg(arg) {
         )
     } else if (c == Object) {
         //判断对象内有没有方法
-        if(isObjHasFunction(arg)){
+        if (isObjHasFunction(arg)) {
             const token = randomString(8)
             objectMap[token] = arg
             return concatArr(
                 num2arr(REQT.OBJECT, 1),
                 textEncoder.encode(token)
             )
-        }else{
+        } else {
             //当成数据对象传递
-            const body=JSON.stringify(arg)
+            const body = JSON.stringify(arg)
             return concatArr(
-                num2arr(REQT.JSON,1),
+                num2arr(REQT.JSON, 1),
                 textEncoder.encode(body)
             )
         }
@@ -174,18 +174,18 @@ function encodeArg(arg) {
             num2arr(REQT.BOOLEAN, 1),
             new Uint8Array([arg ? 1 : 0])
         )
-    } else if (c == Error){
+    } else if (c == Error) {
         return concatArr(
             num2arr(REQT.ERROR, 1),
             textEncoder.encode(arg.toStr())
         )
     } else {
-        throw new Error("Not support type to encode:"+arg)
+        throw new Error("Not support type to encode:" + arg)
     }
 }
 
 //编码参数列表
-function encodeArray(args) {
+function encodeArray (args) {
     let res = new Uint8Array(0)
     for (const a of args) {
         const buf = encodeArg(a)
@@ -199,13 +199,18 @@ function encodeArray(args) {
 }
 
 //解码参数
-function decodeArg(bytes) {
+function decodeArg (bytes, throwErr = true) {
     //先读取一个标志位
     const t = bytes[0]
     const body = bytes.slice(1)
     switch (t) {
         case REST.ERROR:
-            throw new Error(naviveErrorPrefix+textDecoder.decode(body))
+            const err = new Error(naviveErrorPrefix + textDecoder.decode(body))
+            if (throwErr) {
+                throw err
+            } else {
+                return err
+            }
         case REST.NULL:
             return null
         case REST.STRING:
@@ -220,20 +225,20 @@ function decodeArg(bytes) {
                 body.slice(0, 8)
             )
             //field以，分隔以0结尾
-            const index=body.indexOf(0,8)
-            const fieldArr = body.slice(8,index)
-            const methodArr = body.slice(index+1,body.length)
+            const index = body.indexOf(0, 8)
+            const fieldArr = body.slice(8, index)
+            const methodArr = body.slice(index + 1, body.length)
 
             const fieldRaw = textDecoder.decode(fieldArr)
             const funcRaw = textDecoder.decode(methodArr)
 
-            const fieldList = fieldRaw.length==0?[]:fieldRaw.split(',')
-            const funcList = funcRaw.length==0?[]:funcRaw.split(',')
-        
+            const fieldList = fieldRaw.length == 0 ? [] : fieldRaw.split(',')
+            const funcList = funcRaw.length == 0 ? [] : funcRaw.split(',')
+
             const obj = createObject(token, fieldList, funcList)
             //当对象不使用的时候 通知native回收内存
-            if(register){
-                register.register(obj,token)
+            if (register) {
+                register.register(obj, token)
             }
             return obj
         case REST.BYTEARRAY:
@@ -253,12 +258,12 @@ function decodeArg(bytes) {
         case REST.JSON:
             return JSON.parse(textDecoder.decode(body))
         default:
-            throw new Error("Not support type to decode:"+t)
+            throw new Error("Not support type to decode:" + t)
     }
 }
 
 //解码参数数组
-function decodeArray(bytes) {
+function decodeArray (bytes) {
     //arraybuffer
     let buffer = bytes.buffer
     //返回参数
@@ -279,13 +284,13 @@ function decodeArray(bytes) {
 /**
  * 创建一个proxy代理的api
  */
-function createObject(token,fieldList = [], funcList = []) {
-    let src={}
-    fieldList.forEach((name)=>{
-        src[name]=null
+function createObject (token, fieldList = [], funcList = []) {
+    let src = {}
+    fieldList.forEach((name) => {
+        src[name] = null
     })
-    funcList.forEach((name)=>{
-        src[name]=function(){
+    funcList.forEach((name) => {
+        src[name] = function () {
             //调用该方法
             const bytes = encodeArray(arguments)
             const args = Base64.fromUint8Array(bytes)
@@ -300,14 +305,14 @@ function createObject(token,fieldList = [], funcList = []) {
         }
     })
     return new Proxy(src, {
-        get(_, key) {
+        get (_, key) {
             for (const func of funcList) {
                 if (func === key) {
                     return src[key]
                 }
             }
-            for (const field of fieldList){
-                if (field === key){
+            for (const field of fieldList) {
+                if (field === key) {
                     const res = window.zeb.readObject(
                         token,
                         key
@@ -323,7 +328,7 @@ function createObject(token,fieldList = [], funcList = []) {
 /**
  * 处理队列下来的消息
  */
-async function processMessage(bytes,resCallback) {
+async function processMessage (bytes, resCallback) {
     const t = bytes[0]
     const body = bytes.slice(1)
     switch (t) {
@@ -335,7 +340,7 @@ async function processMessage(bytes,resCallback) {
                 if (func) {
                     const argsRaw = body.slice(8)
                     const args = decodeArray(argsRaw)
-                    const res=await func.apply(func, args)
+                    const res = await func.apply(func, args)
                     resCallback(res)
                 }
             }
@@ -343,16 +348,16 @@ async function processMessage(bytes,resCallback) {
         case AREST.OBJECT_CALLBACK:
             {
                 //调用对象内的回调
-                const token=textDecoder.decode(body.slice(0,8))
-                const obj=objectMap[token]
-                if(obj){
-                    const nameEnd=body.indexOf(0,8)
-                    const funcName=textDecoder.decode(body.slice(8,nameEnd))
-                    const func=obj[funcName]
-                    if(func){
-                        const argsRaw=body.slice(nameEnd+1)
-                        const args=decodeArray(argsRaw)
-                        const res = await func.apply(func,args)
+                const token = textDecoder.decode(body.slice(0, 8))
+                const obj = objectMap[token]
+                if (obj) {
+                    const nameEnd = body.indexOf(0, 8)
+                    const funcName = textDecoder.decode(body.slice(8, nameEnd))
+                    const func = obj[funcName]
+                    if (func) {
+                        const argsRaw = body.slice(nameEnd + 1)
+                        const args = decodeArray(argsRaw)
+                        const res = await func.apply(func, args)
                         resCallback(res)
                     }
                 }
@@ -361,29 +366,29 @@ async function processMessage(bytes,resCallback) {
         case AREST.RELEASE_CALLBACK:
             {
                 //释放方法
-                const name=textDecoder.decode(body)
+                const name = textDecoder.decode(body)
                 delete functionMap[name]
             }
             break
         case AREST.RELEASE_OBJECT:
             {
                 //释放对象
-                const name=textDecoder.decode(body)
+                const name = textDecoder.decode(body)
                 delete objectMap[name]
             }
             break
         case AREST.PROMISE_FINISH:
             {
                 //promise finalize
-                const name=textDecoder.decode(body.slice(0,8))
-                const promise=promiseMap[name]
-                if(promise){
-                    const success=body[8]==1
-                    const arg=decodeArg(body.slice(9))
-                    if(success){
+                const name = textDecoder.decode(body.slice(0, 8))
+                const promise = promiseMap[name]
+                if (promise) {
+                    const success = body[8] == 1
+                    const arg = decodeArg(body.slice(9), false)
+                    if (success) {
                         promise.resolve(arg)
-                    }else{
-                        promise.reject(new Error(naviveErrorPrefix+arg))
+                    } else {
+                        promise.reject(new Error(naviveErrorPrefix + arg))
                     }
                     delete promiseMap[name]
                 }
@@ -393,7 +398,7 @@ async function processMessage(bytes,resCallback) {
 }
 
 if (window.zeb) {
-    const baseApi = createObject(null,[],[
+    const baseApi = createObject(null, [], [
         "registerServiceWatcher",
     ], true)
     const objList = baseApi.registerServiceWatcher((name, obj) => {
@@ -404,25 +409,25 @@ if (window.zeb) {
         const obj = objList[i]
         apiInternal[name] = obj
     }
-    api=apiInternal
+    api = apiInternal
 
     // 初始化回调接受器
     // 由于native并不能正确的等待promise 所以需要手动实现回调
-    window.zebCall=async function(str,promiseId){
-        const data=Base64.toUint8Array(str)
-        let resValue=null
-        try{
+    window.zebCall = async function (str, promiseId) {
+        const data = Base64.toUint8Array(str)
+        let resValue = null
+        try {
             await processMessage(
                 data,
-                (res)=>{
-                    resValue=res
+                (res) => {
+                    resValue = res
                 }
             )
-        }catch(e){
+        } catch (e) {
             console.warn(e)
-            resValue=e
+            resValue = e
         }
-        if(promiseId){
+        if (promiseId) {
             window.zeb.finalizePromise(
                 promiseId,
                 Base64.fromUint8Array(encodeArg(resValue))
--- web/main.js
+++ web/main.js
@@ -1,45 +1,49 @@
-import {api} from './lib/zeb'
+import { api } from './lib/zeb'
 
-function $(){
+function $ () {
     return document.querySelector(...arguments)
 }
 
-$('#getAge').onclick=()=>{
+$('#getAge').onclick = () => {
     console.log(api.TestService.age)
 }
-$('#addAge').onclick=()=>{
+$('#addAge').onclick = () => {
     console.log(api.TestService.ageAdd())
 }
-$('#callObj').onclick=()=>{
-    const obj=api.TestService.getInnerObject()
+$('#callObj').onclick = () => {
+    const obj = api.TestService.getInnerObject()
     console.log(obj.innerFunction())
 }
-$('#testType').onclick=async()=>{
-    const res=await api.TestService.testType(
-        0xff,
-        0x5555555555,
-        -10.24,
-        "Hello world",
-        false,
-        new Uint8Array([0x5f,0x68]),
-        null,
-        {name:'Jack',age:18},
-        [
-            "Don't worry",
-            {price:100},
-            [-100,200,-300],
-            "Be Happy"
-        ],
-        function(){
-            console.log(arguments)
-            return "Yes Please"
-        },
-        {
-            done:async(txt)=>{
-                console.log(txt)
-                return 999
+$('#testType').onclick = async () => {
+    try {
+        const res = await api.TestService.testType(
+            0xff,
+            0x5555555555,
+            -10.24,
+            "Hello world",
+            false,
+            new Uint8Array([0x5f, 0x68]),
+            null,
+            { name: 'Jack', age: 18 },
+            [
+                "Don't worry",
+                { price: 100 },
+                [-100, 200, -300],
+                "Be Happy"
+            ],
+            function () {
+                console.log(arguments)
+                return "Yes Please"
+            },
+            {
+                done: async (txt) => {
+                    console.log(txt)
+                    return 999
+                }
             }
-        }
-    )
-    console.log(res)
+        )
+        console.log(res)
+    } catch (e) {
+        console.error("invoke error", e)
+    }
 }
